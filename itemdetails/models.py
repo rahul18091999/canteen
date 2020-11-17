805c926fb2bf249e148d8083af9bebbc19e2bbb6
rahul18091999 <rahulmittalnrw@gmail.com>
2020-11-17 23:02:36 +0530

    push

--- itemdetails/models.py
+++ itemdetails/models.py
@@ -8,7 +8,6 @@ class itemdetail(models.Model):
     name=models.CharField(max_length=32)
     price=models.CharField(max_length=32)
     quantity=models.CharField(max_length=32)
-    #image=models.ImageField(upload_to='pics')
     image=models.CharField(max_length=100)
     desc=models.CharField(max_length=100)
 
